79e531939a757ca1a3fb22946767edf21c1dd65e
Ailton <ailtonflores20@gmail.com>
2020-11-28 17:58:38 -0300

    Header edited

---
+++ src/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./Header.css";
+
+function Header() {
+  return (
+    <div
+    className="header"> <img
+      
+     className="header_logo" 
+     src="https://drive.google.com/file/d/1QFoTyN-nL1fU1XhDQKTjhSln-6FxskdO/view?usp=sharing"
+    />
+    <div
+    className="header_searchImput" type="text"/>
+
+     </div>
+
+  )
+}
+
+export default Header
+
