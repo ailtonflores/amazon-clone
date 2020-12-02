000775be10799199bfcde934afec244c9190a149
Ailton <ailtonflores20@gmail.com>
2020-12-02 13:37:31 -0300

    created home and products files

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     //BEM convention
     <div className="app">
-      <h1>Hi Clevers Programmers, let's build the Amazon Store</h1>
+      
     
     <Header />
     <Home />
--- src/Product.js
+++ src/Product.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import "./Product.css";
 
-function Product() {
+function Product({ id, title, image, price, rating }) {
   return (
     <div className="product">
       <div className='product__info'>
-        <p>The lean startup</p>
+        <p>{title}</p>
         <p className="product__price">
             <small>$</small>
             <srong>19.99</srong>
         </p>
+        <div className="product__rating">
+          <p>*</p>
+          <p>*</p>
+          <p>*</p>
+
+        </div>
+
       </div>
-      
+      <img src="https://images-na.ssl-images-amazon.com/images/I/51zBsISdaJL._SX331_BO1,204,203,200_.jpg"/>
+      <button>Add to Basket</button>
+    
     </div>
   )
 }
